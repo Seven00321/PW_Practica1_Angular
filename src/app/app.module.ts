e180d4618dc1a96ee2a4781dbc98316fe9bfaaae
Seven00321 <richrdpere123@gmail.com>
2023-09-13 17:04:57 -0500

    Primer commit de la practica en angular desde la OFICINA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ContadorComponent,
+    ],
+    imports: [
+        BrowserModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
